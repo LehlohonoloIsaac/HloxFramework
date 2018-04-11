081c494504dcdc66d19577e698cd66025fb2d507
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2018-04-11 10:53:24 +0200

    Finished out HloxFramework

---
+++ HloxFramework/HloxFramework/HloxFramework.h
@@ -0,0 +1,19 @@
+//
+//  HloxFramework.h
+//  HloxFramework
+//
+//  Created by Lehlohonolo Mbele on 2018/04/11.
+//  Copyright © 2018 Lehlohonolo Mbele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HloxFramework.
+FOUNDATION_EXPORT double HloxFrameworkVersionNumber;
+
+//! Project version string for HloxFramework.
+FOUNDATION_EXPORT const unsigned char HloxFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HloxFramework/PublicHeader.h>
+
+
---
+++ HloxFramework/HloxFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  HloxFramework
+//
+//  Created by Lehlohonolo Mbele on 2018/04/11.
+//  Copyright © 2018 Lehlohonolo Mbele. All rights reserved.
+//
+
+import Foundation
+
+class Service {
+    private init() {}
+    
+    public func didSomething() -> String {
+        return "Did some stuff, here you go homeboy"
+    }
+}
